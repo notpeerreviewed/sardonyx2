5c0441a3a2aa145cdd5e32a33c081e4532495d13
Jeff Lean <notpeerreviewed@users.noreply.github.com>
2019-11-04 15:32:40 +1300

    Add files via upload

---
+++ sardonyx_dashboard_v2/server.R
@@ -0,0 +1,14 @@
+
+# server setup for udemy-d3 course
+
+library(dplyr)
+library(magrittr)
+
+library(readxl)
+library(servr)
+
+setwd("C:/Users/leanj/sardonyx_dashboard")
+load("data/incidents.rda")
+
+
+httw()
