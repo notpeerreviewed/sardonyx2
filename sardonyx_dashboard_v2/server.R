2bb1090d58f8e01e159feb75e593216e1a9f3683
Jeff Lean <notpeerreviewed@users.noreply.github.com>
2019-11-07 15:39:54 +1300

    Add files via upload

--- sardonyx_dashboard_v2/server.R
+++ sardonyx_dashboard_v2/server.R
@@ -7,8 +7,8 @@ library(magrittr)
 library(readxl)
 library(servr)
 
-setwd("C:/Users/leanj/sardonyx_dashboard")
-load("data/incidents.rda")
+setwd("C:/Users/leanj/sardonyx_dashboard_v2")
+# load("data/incidents.rda")
 
 
 httw()
